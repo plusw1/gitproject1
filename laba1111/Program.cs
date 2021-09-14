81363fe0c0364a3278f1d0e059a937600497e397
+w <maksaste03@gmail.com>
2021-09-14 23:04:33 +0300

    first commit

---
+++ laba1111/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laba1111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
